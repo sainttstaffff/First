39bd7c5620fab22876c91012808efda25bedcda4
s.vysoczkaya.2022 <s.vysoczkaya.2022@CLOUD-RDHOST1>
2024-01-22 09:02:04 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void go_south_east(int *lat, int *lon)
+{
+	*lat = *lat - 1;
+	*lon = *lon + 1;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int latitude = 32;
+	int longtude = -64;
+	go_south_east(&latitude, &longtude);
+	printf("Стоп!Теперь наши координаты: [%i, %i]|\n",
+		latitude, longtude);
+	return 0;
+}
---
+++ ConsoleApplication222/ConsoleApplication222.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int contestants[] = { 1, 2, 3 };
+	int* choise = contestants;
+
+	contestants[0] = 2;
+	contestants[1] = contestants[2];
+	contestants[2] = *choise;
+
+	printf("Я собираюсь выбрать президента № %i\n", contestants[2]);
+	return 0;
+}
